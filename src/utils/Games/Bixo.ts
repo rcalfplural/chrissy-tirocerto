e2bcb410c1e0534e8f2f8f3db6594438144880df
Bruhsader <53186400+RJOOJ-JS@users.noreply.github.com>
2022-04-30 12:23:46 -0400

    Custom emojis to the embed

--- src/utils/Games/Bixo.ts
+++ src/utils/Games/Bixo.ts
@@ -1,38 +1,41 @@
 interface IAnimalTable {
-    [key: number]: string;
+    [key: number]: IAnimal;
 };
+interface IAnimal{
+    name: string; emoji: string;
+}
 interface IBixoResultReturn{
     dezena: number;
     group: number;
-    animal: string;
+    animal: IAnimal;
 };
 
 const table: IAnimalTable= {
-    1: "Avestruz",
-    2: "Aguia",
-    3: "Burro",
-    4: "Borboleta",
-    5: "Cachorro",
-    6: "Cabra",
-    7: "Carneiro",
-    8: "Camelo",
-    9: "Cobra",
-    10: "Coelho",
-    11: "Cavalo",
-    12: "Elefante",
-    13: "Galo",
-    14: "Gato",
-    15: "Jacare",
-    16: "Leao",
-    17: "Macaco",
-    18: "Porco",
-    19: "Pavao",
-    20: "Peru",
-    21: "Touro",
-    22: "Tigre",
-    23: "Urso",
-    24: "Veado",
-    25: "Vaca"
+    1: { name: "Avestruz", emoji: "<:ostrich:969994921358532609>"},
+    2: { name: "Aguia", emoji: ""},
+    3: { name: "Burro", emoji: "<:donkey:969995406106832906>"},
+    4: { name: "Borboleta", emoji: ":butterfly:"},
+    5: { name: "Cachorro", emoji: ":dog:"},
+    6: { name: "Cabra", emoji: ":goat:"},
+    7: { name: "Carneiro", emoji: ":sheep:"},
+    8: { name: "Camelo", emoji: ":camel:"},
+    9: { name: "Cobra", emoji: ":snake:"},
+    10:{ name:  "Coelho", emoji: ":rabbit:"},
+    11:{ name:  "Cavalo", emoji: ":horse:"},
+    12:{ name:  "Elefante", emoji: ":elephant:"},
+    13:{ name:  "Galo", emoji: ":rooster:"},
+    14:{ name:  "Gato", emoji: ":cat:"},
+    15:{ name:  "Jacare", emoji: ":crocodile:"},
+    16:{ name:  "Leao", emoji: ":lion:"},
+    17:{ name:  "Macaco", emoji: ":monkey:"},
+    18:{ name:  "Porco", emoji: ":pig:"},
+    19:{ name:  "Pavao", emoji: ":peacock:"},
+    20:{ name:  "Peru", emoji: ":turkey:"},
+    21:{ name:  "Touro", emoji: ":ox:"},
+    22:{ name:  "Tigre", emoji: ":tiger:"},
+    23:{ name:  "Urso", emoji: ":bear:"},
+    24:{ name:  "Veado", emoji: ":deer:"},
+    25:{ name:  "Vaca", emoji: ":cow:"}
 };
 
 function CalculateResult(dezena: number): IBixoResultReturn{
@@ -44,4 +47,4 @@ function CalculateResult(dezena: number): IBixoResultReturn{
     return { dezena, group, animal };
 }
 
-export { CalculateResult, table, IBixoResultReturn };
+export { CalculateResult, table, IBixoResultReturn, IAnimal };
--- src/utils/embeds/Loterica.ts
+++ src/utils/embeds/Loterica.ts
@@ -1,6 +1,6 @@
 import { MessageEmbed } from "discord.js";
 import { ILotericaPayload } from "../../definitions";
-import { CalculateResult } from "../Games/Bixo";
+import { CalculateResult, IAnimal } from "../Games/Bixo";
 
 function Generate({ dezenas, gameDate, gameType, gameNumber }:ILotericaPayload): MessageEmbed{
     const embed = new MessageEmbed();
@@ -8,11 +8,12 @@ function Generate({ dezenas, gameDate, gameType, gameNumber }:ILotericaPayload):
     embed.setTitle(`Resultado ${gameType.replace("_", " ")} nº ${gameNumber} - ${gameDate}`);
     embed.addField("Dezenas Sorteadas: ", `**1º** - ${dezenas[0]}\n**2º** - ${dezenas[1]}\n**3º** - ${dezenas[2]}\n**4º** - ${dezenas[3]}\n**5º** - ${dezenas[4]}`);
 
-    const resultsBixo: string[] = [];
+    const resultsBixo: IAnimal[] = [];
     dezenas.map(dez=>{
-        resultsBixo.push(CalculateResult(Number(dez.toString().slice(-2))).animal);
+        const animal = CalculateResult(Number(dez.toString().slice(-2)));
+        resultsBixo.push(animal.animal);
     }); 
-    embed.addField(`Jogo do Bixo Federal`, `**1º** - ${resultsBixo[0]} (cabeça)\n**2º** - ${resultsBixo[1]}\n**3º** - ${resultsBixo[2]}\n**4º** - ${resultsBixo[3]}\n**5º** - ${resultsBixo[4]}\n`);
+    embed.addField(`Jogo do Bixo Federal`, `**1º** - ${resultsBixo[0].name}${resultsBixo[0].emoji} (cabeça)\n**2º** - ${resultsBixo[1].name}${resultsBixo[1].emoji}\n**3º** - ${resultsBixo[2].name}${resultsBixo[2].emoji}\n**4º** - ${resultsBixo[3].name}${resultsBixo[3].emoji}\n**5º** - ${resultsBixo[4].name}${resultsBixo[4].emoji}\n`);
 
     return embed;
 }
